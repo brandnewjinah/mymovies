d00a4fa70ec5ef9fdbd0d9c5a935a6481f885cce
Jinah Lee <brandnewjinah@gmail.com>
2021-12-21 12:54:20 -0800

    Client - Profile - getGenre error track and test

--- client/src/util/getGenres.js
+++ client/src/util/getGenres.js
@@ -1,17 +1,18 @@
 export const getGenre = (genres, genre_ids) => {
   //genre in array like [12, 23, 34]
-  if (genre_ids.some((obj) => Object.keys(obj).includes("id"))) {
-    const currGenres = genre_ids.map((g) => {
-      return g.name;
-    });
-    console.log(currGenres.slice(0, 2));
-    return currGenres.slice(0, 2);
-  } else {
-    const currGenres = genre_ids.map((genreId) => {
-      //get updated genres list from the server, and return genre name that matches genre id
-      const match = genres.find((item) => item.id === genreId);
-      return match.name;
-    });
-    return currGenres.slice(0, 2);
-  }
+  console.log(genre_ids);
+  // if (genre_ids.some((obj) => Object.keys(obj).includes("id"))) {
+  //   const currGenres = genre_ids.map((g) => {
+  //     return g.name;
+  //   });
+  //   console.log(currGenres.slice(0, 2));
+  //   return currGenres.slice(0, 2);
+  // } else {
+  //   const currGenres = genre_ids.map((genreId) => {
+  //     //get updated genres list from the server, and return genre name that matches genre id
+  //     const match = genres.find((item) => item.id === genreId);
+  //     return match.name;
+  //   });
+  //   return currGenres.slice(0, 2);
+  // }
 };
